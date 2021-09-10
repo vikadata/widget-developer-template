51a295ad1ebdfd386240bab0808d1092f94a05ec
陈加贝 <chenjiabei@vikadata.com>
2021-09-10 19:25:47 +0800

    chore: 升级版本，调整目录结构

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { initializeWidget } from '@vika/widget-sdk';
 import { Information } from './information';
 import { Selection } from './selection';
 import { Setting } from './setting';
 import { Control } from './control';
 import { Storage } from './storage';
 
-export const WidgetDeveloperTemplate: React.FC = () => {
+const WidgetDeveloperTemplate: React.FC = () => {
   return (
     <div style={{ display: 'flex', height: '100%' }}>
       <div style={{ flexGrow: 1, overflow: 'auto', padding: '0 8px'}}>
@@ -22,3 +23,6 @@ export const WidgetDeveloperTemplate: React.FC = () => {
     </div>
   );
 };
+
+
+initializeWidget(WidgetDeveloperTemplate, process.env.WIDGET_PACKAGE_ID!);
