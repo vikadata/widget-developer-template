7d430a7308a35fe9ca86c86c440f4a03e205d6a5
陈加贝 <chenjiabei@vikadata.com>
2021-09-30 18:59:08 +0800

    refactor: upgrade useExpandRecord useage

--- src/control.tsx
+++ src/control.tsx
@@ -15,7 +15,7 @@ export const Control: React.FC = () => {
   return (
     <div>
         <h3>组件自身 config 控制与读取</h3>
-        <p>第一个 record: {allRecords[0].getCellValueString(primaryField!.id)} <Button size="small" onClick={() => expandRecord({viewId: view.id, recordId: allRecords[0].id})}>Expand</Button></p>
+        <p>第一个 record: {allRecords[0].getCellValueString(primaryField!.id)} <Button size="small" onClick={() => expandRecord({viewId, recordIds: [allRecords[0].id]})}>Expand</Button></p>
         <p>WidgetFullscreen: {isFullscreen+ ''} <Button size="small" onClick={() => toggleFullscreen()}>Toggle</Button></p>
         <p>
           WidgetSettingOpened（只允许在展开模式下打开）: {isShowingSettings + ''}
